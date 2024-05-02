0869ad583b8f5e8cfa7945bf2ae02deaec480411
adithyansp17 <adithyanmm17@gmail.com>
2024-05-02 09:35:41 +0530

    First commit

---
+++ FoodTOrderWPF/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FoodTOrderWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool fullScreen = false;
+            foreach (string key in e.Args)
+            {
+                if (key == "")
+                    fullScreen = true;
+            }
+
+            this.Properties["Login"] = "DonJoe";
+            this.Properties["Password"] = "pwd";
+
+            
+        }
+    }
+
+}
---
+++ FoodTOrderWPF/SeondWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodTOrderWPF
+{
+    /// <summary>
+    /// Interaction logic for SeondWindow.xaml
+    /// </summary>
+    public partial class SeondWindow : Window
+    {
+        public SeondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem li = (ListBoxItem)lstUsers.SelectedValue;
+            string str = "";
+            if (li.Content is StackPanel) {
+                StackPanel sp = (StackPanel)li.Content;
+                Label l = (Label)sp.Children[0];
+                Button button = (Button)sp.Children[1];
+                str += l.Content.ToString();
+            }
+            MessageBox.Show(str);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
